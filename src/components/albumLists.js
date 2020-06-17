4a83e089cc3eaf55acf70c018af23af35035937c
plueksaporn <plueksaporn.kattiyawong@scb.co.th>
2020-06-17 22:51:50 +0700

    Call api with 'axios' and setState without hooks.

--- src/components/albumLists.js
+++ src/components/albumLists.js
@@ -1,11 +1,17 @@
 import React, {Component} from 'react';
 import {Text, View} from 'react-native';
+import axios from 'axios';
 
 class AlbumLists extends Component {
+  state = {album: []};
   componentDidMount() {
     console.log('componentDidMount!!!');
+    axios
+      .get('https://rallycoding.herokuapp.com/api/music_albums')
+      .then((responseData) => this.setState({album: responseData.data}));
   }
   render() {
+    console.log(this.state.album);
     return (
       <View>
         <Text>Album List!!!!</Text>
